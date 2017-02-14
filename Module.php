eebb666b39ab8a1d9d8d880d285c289c3b5100f1
Wilmer Alvarez <wilmer.alvarez@economiayfinanzas.gob.bo>
2017-02-14 00:25:45 -0400

    add module.php

---
+++ Module.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/).
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
+ *
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace Administracion;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module implements AutoloaderProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__.'/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__.'/src/'.__NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        // You may not need to do this if you're doing it elsewhere in your
+        // application
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+}
